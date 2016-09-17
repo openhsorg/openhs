f4536d1772384e9f05240d32d7f1c000e6f2e01c
"Michal <"michal.valny@openhs.org">
2016-09-18 01:55:39 +0200

    update

---
+++ tools/openhs_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Michal Valny OK
+# Ubuntu installation:
+# sudo apt-get install dos2unix
+# to convert a file:
+# dos2unix myscript.sh
+
+OPENHS="openhs_20160916"
+
+echo "********** OpenHS setupsetup of computer unit (Raspberry Pi )... **********"
+
+wget http://openhs.org/wordpress/wp-content/uploads/$OPENHS.zip
+
+unzip $OPENHS.zip
+
+rm $OPENHS.zip
+
+cd $HOME/"openhs"
+
+chmod +x start.sh
+
+./start.sh
+
