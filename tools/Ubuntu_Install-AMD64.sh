152225e05f9d97b62e0282d4672cb0bb95bd4fa2
Michal Valny <michal.valny@openhs.org>
2017-02-07 10:21:53 +0100

    Update Ubuntu_Install-AMD64.sh

--- tools/Ubuntu_Install-AMD64.sh
+++ tools/Ubuntu_Install-AMD64.sh
@@ -100,8 +100,8 @@ apt-get -y install tor-browser
 #Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update
-sudo apt-get install google-chrome-stable
+apt-get -y update
+sudo apt-get -y install google-chrome-stable
 
 #GIT
 apt-get -y update
@@ -132,6 +132,10 @@ apt-get -y install skype
 wget http://installer.jdownloader.org/JD2SilentSetup_x64.sh
 chmod +x JD2SilentSetup_x64.sh
 ./JD2SilentSetup_x64.sh
+
+#Cryptsetup
+sudo apt-get -y update
+sudo apt-get -y install cryptsetup
 
 #Settings
 echo "********** ---------- Settings... ---------- **********"
