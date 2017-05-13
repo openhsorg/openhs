490c81858d99986c77b997125dc2c62bd7af711b
"Michal <"michal.valny@openhs.org">
2017-05-13 12:08:15 +0200

    sketch updates

--- hardware_units/u19_temperature_sensor_wemos_dht22/sketch_u19_temp_sensor/sketch_u19_temp_sensor.ino
+++ hardware_units/u19_temperature_sensor_wemos_dht22/sketch_u19_temp_sensor/sketch_u19_temp_sensor.ino
@@ -1,3 +1,9 @@
+/*
+ * OpenHS Wifi Temperature Sensor
+ * Michal Valny, michal.valny@openhs.org
+ * May-2017
+ */
+
 #include <ESP8266WiFi.h>
 #include <Homie.h>
 #include "FS.h"
@@ -15,7 +21,6 @@ DHT dht(DHTPIN, DHTTYPE);
 HomieNode temperatureNode("temperature", "temperature");
 
 void format () {
-
   SPIFFS.format();    
   delay(500);
   ESP.reset();  
@@ -37,9 +42,7 @@ bool messageHandler(String value) {
 }  
 
 void setupHandler() {
-
   dht.begin();
-  
   Homie.setNodeProperty(temperatureNode, "unit", "c", true);      
 }
 
@@ -50,32 +53,21 @@ void loopHandler () {
     float humidity = dht.readHumidity();
     float temperature = dht.readTemperature();
 
-
-    humidity = dht.readHumidity();
-    temperature = dht.readTemperature();    
-  
-   // float temperature = 22; // Fake temperature here, for the example
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" °C");
+    if (isnan(humidity) || isnan(temperature)) {
+      return;
+    }     
+    
     if (Homie.setNodeProperty(temperatureNode, "degrees", String(temperature), true)) {
       lastTemperatureSent = millis();
-    } else {
-      Serial.println("Temperature sending failed");
     }
 
     if (Homie.setNodeProperty(temperatureNode, "humidity", String(humidity), true)) {
       lastTemperatureSent = millis();
-    } else {
-      Serial.println("Temperature sending failed");
     }
-    
   }  
 }
 
-
 void setup() {
-
   Homie.setFirmware("OpenHS temperature", "1.0.0");
   Homie.registerNode(temperatureNode);
   temperatureNode.subscribe("message", messageHandler);    
