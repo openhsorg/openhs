51a927b09f9a44b0fb5f28ec9048ca5460c41130
"Michal <"michal.valny@openhs.org">
2017-05-24 00:02:05 +0200

    update

--- modules/org.openhs.core.cfg/openhs/setup.sh
+++ modules/org.openhs.core.cfg/openhs/setup.sh
@@ -18,8 +18,6 @@ config_file=$config_dir"/FILE.cfg"
 
 echo "---------- Setup OpenHS ----------"
 
-<<"COMMENT"
-
 #Get list of files...
 cd $plugins_dir
 yourfilenames=`ls $plugins_dir/*.jar`
@@ -32,7 +30,19 @@ do
   #echo `basename "$eachfile"`
   #echo $(basename $eachfile)
   ((num_files++))
+
+  if [[ $eachfile == *"org.eclipse.osgi_"* ]]
+  then
+     #echo "It's there!"$eachfile
+    # osgi_file = "$eachfile"
+
+     osgi_file=" ${eachfile}${osgi_file}"
+
+  fi
+
 done
+
+ #echo "It's there!"$osgi_file
 
 ###Write config file
 #Delete config dir...
@@ -76,13 +86,13 @@ do
 done
 
 echo "OpenHS config file created..."
-COMMENT
+
 
 service_file="/lib/systemd/system/openhs.service"
 echo "Platform is $(uname -s)"
 _JAVAC_LOCATION=`type java | cut -f3 -d' '`
-#echo "Java is: $_JAVA_LOCATION"
 echo "Setup OpenHS service.."
+
 
 if [ -f "$service_file" ]; then
   rm "$service_file" -r
@@ -97,7 +107,7 @@ echo "Type=simple" >> $service_file
 echo "User=root" >> $service_file
 echo "Group=root" >> $service_file
 echo "WorkingDirectory=$openh_dir" >> $service_file
-echo "ExecStart=$_JAVA_LOCATION -jar $plugins_dir/org.eclipse.osgi_3.11.3.v20170209-1843.jar -console -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=7070 -Dorg.eclipse.equinox.http.jetty.context.sessioninactiveinterval=0 -Xmx700m" >> $service_file
+echo "ExecStart=$_JAVA_LOCATION -jar $osgi_file -console -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=7070 -Dorg.eclipse.equinox.http.jetty.context.sessioninactiveinterval=0 -Xmx700m" >> $service_file
 echo "StandardOutput=syslog" >> $service_file
 echo "StandardError=syslog" >> $service_file
 echo "RestartSec=5" >> $service_file
@@ -106,5 +116,47 @@ echo "" >> $service_file
 echo "[Install]" >> $service_file
 echo "WantedBy=multi-user.target" >> $service_file
 
+#MQTT mosquitto install..
+if ! [ -x "$(command -v mosquitto)" ]; then
+  echo 'MQTT Mosquitto is NOT installed, continue with this..' >&2
+  apt-get update -y -qq
+  apt-get install -y -qq mosquitto mosquitto-clients
+#sudo /etc/init.d/mosquitto start
+  sudo /etc/init.d/mosquitto stop
 
-echo "---------- Setup OpenHS Done, enjoy! ----------"
+  echo "Setup mosquitto.conf file.."
+
+  mosquito_conf="/etc/mosquitto/mosquitto.conf"
+
+  if [ -f "$mosquito_conf" ]; then
+    rm "$mosquito_conf" -r
+  fi
+
+  echo "# Mosquitto config..." > $mosquito_conf
+  echo "pid_file /var/run/mosquitto.pid" >> $mosquito_conf
+  echo "" >> $mosquito_conf
+  echo "persistence true" >> $mosquito_conf
+  echo "persistence_location /var/lib/mosquitto/" >> $mosquito_conf
+  echo "" >> $mosquito_conf
+  echo "log_dest topic" >> $mosquito_conf
+  echo "" >> $mosquito_conf
+  echo "log_type error" >> $mosquito_conf
+  echo "log_type warning" >> $mosquito_conf
+  echo "log_type notice" >> $mosquito_conf
+  echo "log_type information" >> $mosquito_conf
+  echo "" >> $mosquito_conf
+  echo "connection_messages true" >> $mosquito_conf
+  echo "log_timestamp true" >> $mosquito_conf
+  echo "" >> $mosquito_conf
+  echo "include_dir /etc/mosquitto/conf.d" >> $mosquito_conf
+
+  sudo /etc/init.d/mosquitto start
+
+else
+  echo 'MQTT Mosquitto is installed.' >&2
+  sudo /etc/init.d/mosquitto start
+fi
+
+#IQRF daemon install..
+
+echo "---------- Setup OpenHS done, enjoy! ----------"
