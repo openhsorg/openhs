017dbc541dcb4486d245e05a916e99dc804a85a0
"Michal <"michal.valny@openhs.org">
2017-05-28 15:27:27 +0200

    endpoint coord sys

--- modules/org.openhs.apps.cobalt/res/CobaltScript.ts
+++ modules/org.openhs.apps.cobalt/res/CobaltScript.ts
@@ -10,13 +10,13 @@ class ThreeJSTest {
         
         
             this.scene = new THREE.Scene();
-            this.scene.add( new THREE.GridHelper( 1000, 100 ) );
+            this.scene.add( new THREE.GridHelper( 2000, 30 ) );
         
             //Camera
             var SCREEN_WIDTH = window.innerWidth, SCREEN_HEIGHT = window.innerHeight;
-            var VIEW_ANGLE = 45, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
+            var VIEW_ANGLE = 50, ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT, NEAR = 0.1, FAR = 20000;
             this.camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
-            this.camera.position.set(1200, 1200, 1200);
+            this.camera.position.set(1800, 1800, 1800);
         
             this.camera.lookAt(new THREE.Vector3(0,0,0));
             //this.camera.lookAt(this.scene.position);  
@@ -60,7 +60,19 @@ class ThreeJSTest {
             lineGeometry.computeLineDistances();
             var lineMaterial = new THREE.LineBasicMaterial( { color: 0x0066ff } );
             var line = new THREE.Line( lineGeometry, lineMaterial );
-            this.scene.add(line);          
+            this.scene.add(line);        
+        
+        
+            var sphereGeometry = new THREE.SphereGeometry( 10, 32, 16 ); 
+            // use a "lambert" material rather than "basic" for realistic lighting.
+            //   (don't forget to add (at least one) light!)
+            var sphereMaterial = new THREE.MeshBasicMaterial( {color: 0x8888ff} ); 
+            var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+            sphere.position.set(780, 1000, 0);
+            this.scene.add(sphere);        
+        
+        
+            this.drawEndCS(780, 1000, 0, 50);
 
             this.renderer.render(this.scene, this.camera);     
 
@@ -71,7 +83,38 @@ class ThreeJSTest {
     start() {
         //this.renderer.clear();
     }
+    
+    public drawEndCS (x: number, y: number, z: number, lenght: number) {
+                    //Line X
+            var lineGeometry = new THREE.Geometry();
+            var vertArray = lineGeometry.vertices;
+            vertArray.push( new THREE.Vector3(x, y, z), new THREE.Vector3(x + lenght, y, z) );
+            lineGeometry.computeLineDistances();
+            var lineMaterial = new THREE.LineBasicMaterial( { color: 0xcc0000 } );
+            var line = new THREE.Line( lineGeometry, lineMaterial );
+            this.scene.add(line);
+        
+            //Line Y
+            var lineGeometry = new THREE.Geometry();
+            var vertArray = lineGeometry.vertices;
+            vertArray.push( new THREE.Vector3(x, y, z), new THREE.Vector3(x, y + lenght, z) );
+            lineGeometry.computeLineDistances();
+            var lineMaterial = new THREE.LineBasicMaterial( { color: 0x009933 } );
+            var line = new THREE.Line( lineGeometry, lineMaterial );
+            this.scene.add(line);    
+        
+            //Line Z
+            var lineGeometry = new THREE.Geometry();
+            var vertArray = lineGeometry.vertices;
+            vertArray.push( new THREE.Vector3(x, y, z), new THREE.Vector3(x, y, z + lenght) );
+            lineGeometry.computeLineDistances();
+            var lineMaterial = new THREE.LineBasicMaterial( { color: 0x0066ff } );
+            var line = new THREE.Line( lineGeometry, lineMaterial );
+            this.scene.add(line);    
+    }
+
 }
+
 
 window.onload = () => {
     var three = new ThreeJSTest();
