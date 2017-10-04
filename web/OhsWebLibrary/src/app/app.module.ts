bbf8b3c97fc6906e75369721346147d4671e09bc
MichalValny <michal.valny@openhs.org>
2017-10-04 19:39:42 +0200

    update

--- web/OhsWebLibrary/src/app/app.module.ts
+++ web/OhsWebLibrary/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OhsSiteDataModule } from './ohs-site-data/ohs-site-data.module';
+import { OhsGuiFrameworkComponent } from './ohs-gui-framework/ohs-gui-framework.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OhsGuiFrameworkComponent
   ],
   imports: [
     BrowserModule,
---
+++ web/OhsWebLibrary/src/app/ohs-gui-framework/rect.ts
@@ -0,0 +1,82 @@
+export class Rect {
+
+    public x = 0;
+    public y = 0;
+    public w = 0;
+    public h = 0;
+
+    public isClicked (clx:number, cly:number) {
+        if (!(clx > this.x && clx < this.x + this.w)) return false;
+        if (!(cly < this.y+this.h && cly > this.y)) return false;
+
+        return true;
+    }
+
+    public equals (rectI: Rect) {
+        this.x = rectI.x;
+        this.y = rectI.y;
+        this.w = rectI.w;
+        this.h = rectI.h;
+    }
+
+    public size (x: number, y: number, w: number, h: number) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public position (x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public sizeRect (rect: Rect) {
+        this.x = rect.x;
+        this.y = rect.y;
+        this.w = rect.w;
+        this.h = rect.h;
+    }
+
+    public getSize () {
+        const rect: Rect = new Rect ();
+
+        rect.x = this.x;
+        rect.y = this.y;
+        rect.w = this.w;
+        rect.h = this.h;
+
+        return rect;
+    }
+
+    public move (dx: number, dy: number) {
+        this.x = this.x + dx;
+        this.y = this.y + dy;
+    }
+
+    public scaleSize (perc: number) {
+
+        var old_w: number = this.w;
+        var old_h: number = this.h;
+
+        this.w = this.w * perc;
+        this.h = this.h * perc;
+
+        this.x = this.x + ((old_w - this.w) / 2);
+        this.y = this.y + ((old_h - this.h) / 2);
+    }
+
+    public paint(ctx: CanvasRenderingContext2D) {
+      ctx.beginPath();
+      ctx.rect(this.x, this.y, this.w, this.h);
+      ctx.closePath();              
+    }       
+    
+    public getRight () {
+        return this.x + this.w;
+    }
+    
+    public getBottom () {
+        return this.y + this.h;
+    }    
+}
